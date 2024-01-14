fc19cd6b74b51547c94fa6a14dbb1ef31747d1ad
Jackrao-Git <108682585+Jackrao-Git@users.noreply.github.com>
2024-01-13 22:23:42 -0500

    Update test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -3,12 +3,13 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 
 class FlaskAppTests(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path="./webdriver/chromedriver")
+        service = Service(executable_path="./webdriver/chromedriver")
+        self.driver = webdriver.Chrome(service=service)
 
 
 
