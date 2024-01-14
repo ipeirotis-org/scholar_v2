5c8beed19f5b2f10d8deec99f93b811241045687
Jackrao-Git <108682585+Jackrao-Git@users.noreply.github.com>
2024-01-14 16:15:11 -0500

    Update test_3.py

--- tests/test_3.py
+++ tests/test_3.py
@@ -1,10 +1,13 @@
 import unittest
 import random
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+
 
 class DownloadCsvTests(unittest.TestCase):
     def setUp(self):
@@ -17,20 +20,27 @@ class DownloadCsvTests(unittest.TestCase):
 
     def test_download_csv(self):
         self.driver.get("https://scholar.ipeirotis.org/")
-        search_terms = ["Panos Ipeirotis", "Adam Heller", "Juliana Freire"]
+        search_terms = ["Panos Ipeirotis", "Adam Heller", "Juliana Freire", "Claudio T. Silva"]
         search_query = random.choice(search_terms)
-
+    
         search_box = self.driver.find_element(By.NAME, "author_name")
         search_box.clear()
         search_box.send_keys(search_query)
         search_box.send_keys(Keys.RETURN)
 
-        self.driver.implicitly_wait(60)  # Adjust time as needed
+        WebDriverWait(self.driver, 120).until(
+            EC.presence_of_element_located((By.ID, "results"))
+        )
 
-        # Construct download link text based on the author's name
+   
+        
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        
         download_link_text = f"Download CSV for {search_query}"
-        download_link = self.driver.find_element(By.LINK_TEXT, download_link_text)
-      
+        download_link = WebDriverWait(self.driver, 20).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, download_link_text))
+        )
+        download_link.click()
 
     def tearDown(self):
         self.driver.quit()
