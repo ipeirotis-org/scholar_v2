74132f1a0b1dd2bdc5f2f5227755c5738dd335f7
Panos Ipeirotis <ipeirotis@gmail.com>
2024-01-18 16:44:29 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,72 +24,6 @@ app.secret_key = "secret-key"
 app.config["CACHE_TYPE"] = "simple"
 cache = Cache(app)
 
-search_history_keys = []
-
-
-
-# @app.route('/cache_author_name', methods=['POST'])
-# def cache_author_name():
-#    author_name = request.json.get('author_name')
-#    if author_name:
-#        logging.info(f"Caching author name: {author_name}")
-#        check_and_add_author_to_cache(author_name)
-#        return jsonify({'success': True, 'message': 'Author name cached successfully'})
-#    else:
-#        return jsonify({'success': False, 'message': 'No author name provided'})
-
-
-
-@app.route("/get_similar_authors")
-def get_similar_authors():
-    author_name = request.args.get("author_name")
-    authors = []
-
-    # Primary search attempt
-    primary_search = scholarly.search_author(author_name)
-    for _ in range(10):
-        try:
-            author = next(primary_search)
-            if author:
-                authors.append(author)
-        except StopIteration:
-            break  # Break the loop if there are no more authors
-
-    # Process and return the authors
-    clean_authors = [{
-        "name": author.get("name", ""),
-        "affiliation": author.get("affiliation", ""),
-        "email": author.get("email", ""),
-        "citedby": author.get("citedby", 0),
-        "scholar_id": author.get("scholar_id", "")
-    } for author in authors]
-
-    return jsonify(clean_authors)
-
-
-
-
-@app.after_request
-def add_header(response):
-    response.cache_control.no_store = True
-    return response
-
-
-def filter_data_by_timeframe(data, start_year, end_year):
-    return {year: data[year] for year in data if start_year <= year <= end_year}
-
-
-@app.route("/")
-def index():
-    author_count = request.args.get("author_count", default=1, type=int)
-    return render_template("index.html", author_count=author_count)
-
-
-@app.route("/set_author_count", methods=["POST"])
-def set_author_count():
-    author_count = request.form.get("author_count", default=1, type=int)
-    return redirect(url_for("index", author_count=author_count))
-
 
 def perform_search_by_id(scholar_id):
     author, query, total_publications = get_author_statistics_by_id(scholar_id)
@@ -116,8 +50,41 @@ def perform_search_by_id(scholar_id):
 
 
 
+@app.route("/")
+def index():
+    author_count = request.args.get("author_count", default=1, type=int)
+    return render_template("index.html", author_count=author_count)
+
+
+@app.route("/get_similar_authors")
+@cache.cached(timeout=7*24*3600)  # cache for 1 week
+def get_similar_authors():
+    author_name = request.args.get("author_name")
+    authors = []
+
+    # Primary search attempt
+    primary_search = scholarly.search_author(author_name)
+    for _ in range(10):
+        try:
+            author = next(primary_search)
+            if author:
+                authors.append(author)
+        except StopIteration:
+            break  # Break the loop if there are no more authors
+
+    # Process and return the authors
+    clean_authors = [{
+        "name": author.get("name", ""),
+        "affiliation": author.get("affiliation", ""),
+        "email": author.get("email", ""),
+        "citedby": author.get("citedby", 0),
+        "scholar_id": author.get("scholar_id", "")
+    } for author in authors]
+
+    return jsonify(clean_authors)
 
 @app.route("/results", methods=["GET"])
+@cache.cached(timeout=7*24*3600)  # cache for 1 week
 def results():
     author_id = request.args.get("author_id", "")
 
@@ -136,6 +103,12 @@ def results():
     return render_template("results.html", authors_data=authors_data, time_stamp=timestamp, author_count=1)
 
 
+
+
+@app.after_request
+def add_header(response):
+    response.cache_control.no_store = True
+    return response
 
 
 @app.route('/download/<author_id>')
@@ -160,11 +133,6 @@ def download_results(author_id):
         file_path, as_attachment=True, download_name=f"{author_id}_results.csv"
     )
 
-
-def save_to_cache(key, data):
-    cache.set(key, data)
-    if key not in search_history_keys:
-        search_history_keys.append(key)
 
 
 @app.route("/error")
