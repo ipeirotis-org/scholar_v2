0e725afb1a03ad98d5ca7559378c2332edfd0e01
Panos Ipeirotis <ipeirotis@gmail.com>
2024-02-12 17:11:13 +0000

    Adding tasks in requirements

--- functions/main.py
+++ functions/main.py
@@ -8,6 +8,7 @@ import logging
 from datetime import datetime
 import pytz
 from google.cloud import firestore
+from google.cloud import tasks_v2
 
 db = firestore.Client()
 
@@ -82,6 +83,8 @@ def search_author_id(request):
     return response, 200
 
 
+
+
 @functions_framework.http
 def fill_publication(request):
     """HTTP Cloud Function.
@@ -102,7 +105,7 @@ def fill_publication(request):
 
     pub = fill_pub(pub)
     if pub is None:
-        return "Error fill pblication from Google Scholar", 500
+        return "Error fill publication from Google Scholar", 500
 
     response = make_response(pub)
     response.headers["Content-Type"] = "application/json"
@@ -125,38 +128,34 @@ def convert_integers_to_strings(data):
 
 
 def get_author(author_id):
+
     try:
+
         logging.info(f"Fetching author entry for {author_id}")
         author = scholarly.search_author_id(author_id)
         author = scholarly.fill(author)
 
+        
         for pub in author["publications"]:
-            url = "https://northamerica-northeast2-scholar-version2.cloudfunctions.net/fill_publication"
+            url = 'https://northamerica-northeast2-scholar-version2.cloudfunctions.net/fill_publication'
             task = {
                 "http_request": {
                     "http_method": tasks_v2.HttpMethod.POST,
                     "url": url,
-                    "headers": {"Content-type": "application/json"},
-                    "body": f'{{"pub": "{pub}", "use_cache": {use_cache}}}'.encode(),
+                    'headers': {'Content-type': 'application/json'},
+                    'body': f'{{"pub": "{pub}", "use_cache": {use_cache}}}'.encode()
                 }
             }
             response = client.create_task(request={"parent": pubs_queue, "task": task})
 
         # Keep only the IDs and num_citations of the publications, to save space
-        author["publications"] = [
-            {
-                "author_pub_id": pub["author_pub_id"],
-                "num_citations": pub["num_citations"],
-                "filled": False,
-            }
-            for pub in author["publications"]
-        ]
+        author["publications"] = [{"author_pub_id": pub['author_pub_id'], "num_citations": pub['num_citations'], "filled": False} for pub in author["publications"]]
 
         # We leave the co-authors as-is. They tend to be short and we need names and affiliations
-
+        
         serialized = convert_integers_to_strings(json.loads(json.dumps(author)))
 
-        """
+        '''
         while True:
             serialized = convert_integers_to_strings(json.loads(json.dumps(author)))
             # This is a hack to deal with the fact that cache can only hold 0.5Mb docs
@@ -166,15 +165,16 @@ def get_author(author_id):
                 author["publications"] = author["publications"][:half]
             else:
                 break
-        """
+        '''
 
         try:
             set_firestore_cache("scholar_raw_author", author_id, serialized)
-
+            
         except:
             logging.error(f"Error storing author entry {author_id} in Firebase: {e}")
-
+            
         return serialized
+
 
     except Exception as e:
         logging.error(f"Error fetching detailed author data for {author_id}: {e}")
@@ -182,6 +182,7 @@ def get_author(author_id):
 
 
 def fill_pub(pub):
+
     try:
         logging.info(f"Fetching pub entry for publication {pub['author_pub_id']}")
         pub = scholarly.fill(pub)
@@ -191,7 +192,9 @@ def fill_pub(pub):
         return serialized
 
     except Exception as e:
-        logging.error(
-            f"Error fetching detailed data for publication {pub['author_pub_id']}: {e}"
-        )
+        logging.error(f"Error fetching detailed data for publication {pub['author_pub_id']}: {e}")
         return None
+
+
+
+
