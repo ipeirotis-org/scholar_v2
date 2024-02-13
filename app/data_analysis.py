e2ed5920dd19d67947fc407709aaa32dc8885a81
Panos Ipeirotis <ipeirotis@gmail.com>
2024-02-13 21:11:45 +0000

    Disabling caching for stats

--- app/data_analysis.py
+++ app/data_analysis.py
@@ -92,19 +92,19 @@ def get_author_stats(author_id):
     if not author: 
         return None
 
-    author_pub_stats = get_firestore_cache("author_pub_stats",author_id)
-    if not author_pub_stats:
-        author_pub_stats = get_author_pub_stats_bg(author_id)
-        if len(author_pub_stats) == 0: return None
-        set_firestore_cache("author_pub_stats",author_id,author_pub_stats)
+    #author_pub_stats = get_firestore_cache("author_pub_stats",author_id)
+    #if not author_pub_stats:
+    author_pub_stats = get_author_pub_stats_bg(author_id)
+    if len(author_pub_stats) == 0: return None
+    set_firestore_cache("author_pub_stats",author_id,author_pub_stats)
     author['publications'] = author_pub_stats
 
     
-    author_stats = get_firestore_cache("author_stats",author_id)
-    if not author_stats:
-        author_stats = get_author_stats_bg(author_id)
-        if not author_stats: return None
-        set_firestore_cache("author_stats",author_id,author_stats)
+    #author_stats = get_firestore_cache("author_stats",author_id)
+    #if not author_stats:
+    author_stats = get_author_stats_bg(author_id)
+    if not author_stats: return None
+    set_firestore_cache("author_stats",author_id,author_stats)
     author['stats'] = author_stats
 
 
