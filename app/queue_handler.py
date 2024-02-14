6d5b644ce3d8055e730a0a20e3b67c1507025c4e
Panos Ipeirotis <ipeirotis@gmail.com>
2024-02-14 18:18:45 +0000

    More options to select randomly

--- app/queue_handler.py
+++ app/queue_handler.py
@@ -120,7 +120,7 @@ def get_authors_to_fix(num_authors=10):
           `scholar-version2.firestore_views.debug_authors_missing_pubs_in_db`
         WHERE
           pubs_in_pubsdb = 0
-        LIMIT 50
+        LIMIT 200
     """
 
     query_job = bq.query(QUERY)
