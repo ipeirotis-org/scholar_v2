f9723a7dcbf645629b7155e769d48a4ec45bc1b2
Panos Ipeirotis <ipeirotis@gmail.com>
2024-02-19 14:50:51 +0000

    Adding only authors with error above 30%

--- app/queue_handler.py
+++ app/queue_handler.py
@@ -123,7 +123,7 @@ def get_authors_to_fix(num_authors=10):
         FROM
           `scholar-version2.firestore_views.debug_authors_missing_pubs_in_db`
         WHERE
-          err >0.1 
+          err >0.3
         LIMIT 200
     """
 
