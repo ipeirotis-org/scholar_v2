926a3e02fc29ed83e3fdaa84de2558176e05bc55
Panos Ipeirotis <ipeirotis@gmail.com>
2024-02-29 18:33:46 +0000

    Queue handler bug

--- app/queue_handler.py
+++ app/queue_handler.py
@@ -23,4 +23,4 @@ def pending_tasks(author_id):
     return task_queue_service.check_pending_tasks(author_id)
 
 def number_of_tasks_in_queue():
-    return task_queue_service.get_number_of_tasks_in_queue(self)
+    return task_queue_service.get_number_of_tasks_in_queue()
