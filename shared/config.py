6aeddc769417dea2b9bd6641bc452927174cfa8b
Panos Ipeirotis <ipeirotis@gmail.com>
2025-04-29 21:05:15 +0000

    Dynamically switching region based on the day

--- shared/config.py
+++ shared/config.py
@@ -6,13 +6,14 @@ import datetime
 AVAILABLE_FUNCTION_REGIONS = [
     "us-central1", 
     "us-east1", 
-    "us-east4", 
+    "us-east4",
+    "us-east5",
     "us-south1", 
-    "us-west1"
+    "us-west1",
+    "us-west2",
+    "us-west3",
+    "us-west4"
 ]
-
-
-
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
@@ -31,14 +32,14 @@ class Config:
     FIRESTORE_COLLECTION_AUTHOR = "scholar_raw_author"
     FIRESTORE_COLLECTION_PUB = "scholar_raw_pub"
 
-   # --- Dynamic Function Location based on Day ---
+    # --- Dynamic Function Location based on Day ---
     @staticmethod
     def get_daily_region():
         """Selects a region from the available pool based on the day of the year."""
         today = datetime.date.today()
         day_of_year = today.timetuple().tm_yday
         region_index = (day_of_year - 1) % len(AVAILABLE_FUNCTION_REGIONS) # Cycle through regions
-        return AVAILABLE_FUNCTION_REGIONS[region_index]    
+        return AVAILABLE_FUNCTION_REGIONS[region_index]
 
     FUNCTION_LOCATION = get_daily_region()
     print(f"Config: Using Function Location for today: {FUNCTION_LOCATION}") # for logging/debugging
